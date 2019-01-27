e14e98ef82da57e1df29dd1fa8754fe0baf52788
Nick Pettyjohn <n.pettyjohn@gmail.com>
2019-01-26 21:20:43 -0800

    Player and turrent placement
    Implemented more of the player class and turrent placement stuff

--- GGJ2019Tiled/Assets/Scripts/TurretPlacementController.cs
+++ GGJ2019Tiled/Assets/Scripts/TurretPlacementController.cs
@@ -4,21 +4,58 @@ using UnityEngine;
 
 public class TurretPlacementController : MonoBehaviour
 {
-    private int state = 0;
+    enum PlacementState { None, Positioning, Rotating };
+
+    public float placeSpeed = 7.0f;
+
+    private PlacementState state = PlacementState.None;
+
+    //Guardian guardian;
+    //RigidBody2d gBod;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartPlacement(/*Guardian g*/)
     {
-        
+        //guardian = g;
+        Debug.Log("TurretPlacementController -> Starting Placement!");
+
+        state = PlacementState.Positioning;
     }
 
-    public void StartPlacement()
+    public void UpdatePlacement(Vector2 input)
     {
-        Debug.Log("TurretPlacementController -> Starting Placement!");
+        switch(state)
+        {
+            case (PlacementState.Positioning):
+                UpdatePositioning(input);
+                break;
+            case (PlacementState.Rotating):
+                UpdateRotating(input);
+                break;
+        }
+    }
+
+    void UpdatePositioning(Vector2 input)
+    {
+        // move it
+        Vector2 force = input * placeSpeed;
+
+
+        //gBod.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * placeSpeed, 0.8f),
+        //                                        Mathf.Lerp(0, Input.GetAxis("Vertical") * placeSpeed, 0.8f));
+    }
+
+    void UpdateRotating(Vector2 input)
+    {
+        // rotate to look direction 
+        Vector3 look = new Vector3(input.x, input.y, 0.0f);
+
+        float rot_z = Mathf.Atan2(look.y, look.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 }
