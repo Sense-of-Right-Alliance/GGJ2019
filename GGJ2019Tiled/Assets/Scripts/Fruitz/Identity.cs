aea2cad2e18a2dacc8fb70c24e7253b8b601c0c8
mike1072 <mike1072@gmail.com>
2019-01-26 17:31:29 -0800

    add FruitType and Identity

---
+++ GGJ2019Tiled/Assets/Scripts/Fruitz/Identity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Fruitz
+{
+    public class Identity
+    {
+        private Identity() { }
+
+        public string Name { get; set; }
+        public FruitType Type { get; set; }
+        public int Score { get; set; }
+
+        public Identity GenerateNewIdentity()
+        {
+            var type = SelectRandomType();
+            var name = GenerateRandomName(type);
+            return new Identity() { Type = type, Name = name, Score = 0 };
+        }
+
+        private static FruitType SelectRandomType()
+        {
+            return FruitType.Orange;
+        }
+
+        private static string GenerateRandomName(FruitType type)
+        {
+            return "Orangey Porangey";
+        }
+    }
+}
